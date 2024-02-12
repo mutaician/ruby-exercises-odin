0e918f752520670220725fe83ce6407995f7338b
mutaician <109066669+mutaician@users.noreply.github.com>
2024-02-12 10:13:50 +0300

    encapsulate the game using classes

--- Hangman/hangman.rb
+++ Hangman/hangman.rb
@@ -1,67 +1,78 @@
-# Hangman
+class Hangman
+  attr_reader :secret_word, :guessed_letters, :remaining_guesses
 
-# Read words from text file into an array
-def read_words_from_file(filepath)
-  File.readlines(filepath, chomp: true)
-end
-
-# Select random word from array
-def select_random_word(words)
-  filtered_words = words.select { |word| word.length >= 5 && word.length <= 12 }
-  filtered_words.sample.downcase
-end
-
-# display current state of the word with guessed leters revealed
-def display_word(secret_word, guessed_letters)
-  displayed_word = secret_word.chars.map do |letter|
-    guessed_letters.include?(letter)? letter : "_"
+  def initialize(words_file_path)
+    @words = read_words_from_file(words_file_path)
+    @secret_word = select_random_word()
+    @guessed_letters = []
+    @remaining_guesses = 6
   end
-  puts "Secret word: #{displayed_word.join(' ')}"
-end
 
+  def read_words_from_file(file_path)
+    File.readlines(file_path, chomp: true)
+  end
 
-def main_game
-  words = read_words_from_file("words.txt")
-  secret_word = select_random_word(words)
-  p secret_word
+  def select_random_word()
+    filtered_words = @words.select { |word| word.length >= 5 && word.length <= 12 }
+    filtered_words.sample.downcase
+  end
 
-  guessed_letters = []
-  remaining_guesses = 7
-  puts "Welcome to Hangman! Try to guess the secret word."
-  puts "The word has #{secret_word.length} letters."
+  def display_word
+    displayed_word = @secret_word.chars.map do |char|
+      @guessed_letters.include?(char) ? char : '_'
+    end
+    puts "Secret word: #{displayed_word.join(' ')}"
+  end
 
-  loop do
-    display_word(secret_word, guessed_letters)
-    puts "Enter a letter guess: "
-    guess = gets.chomp.downcase
-    # validate user input
+  def handle_guess(guess)
     unless ('a'..'z').include?(guess) && guess.length == 1
-      puts "Invalid guess. Please enter a single letter."
-      next
+      puts "Please enter a single letter."
+      return
     end
 
-    if guessed_letters.include?(guess)
-      puts "You already guessed that letter."
-      next
+    if @guessed_letters.include?(guess)
+      puts "You've already guessed that letter."
+      return
     end
-    # update guessed letters
-    guessed_letters << guess
-    # check for correct guess
-    if secret_word.include?(guess)
-      puts 'Correct guess!'
+
+    @guessed_letters << guess
+
+    if @secret_word.include?(guess)
+      puts "Correct guess!"
     else
-      remaining_guesses -= 1
-      puts "Incorrect guess. You have #{remaining_guesses} guesses remaining."
+      puts "Incorrect guess!"
+      @remaining_guesses -= 1
     end
-    # check for wins or losses
-    if secret_word.chars.all? { |letter| guessed_letters.include?(letter) }
-      puts "Congratulations! You've guessed the word: #{secret_word}"
-      break
-    elsif remaining_guesses.zero?
-      puts "Sorry You've run out of guesses. The word was: #{secret_word}"
-      break
+  end
+
+  def game_over?
+    @secret_word.chars.all? { |char| @guessed_letters.include?(char) } || @remaining_guesses.zero?
+  end
+
+  def display_result
+    if @secret_word.chars.all? { |char| @guessed_letters.include?(char) }
+      puts "Congratulations! You've guessed the word: #{@secret_word}"
+    elsif @remaining_guesses.zero?
+      puts "Sorry, you've run out of guesses. The word was: #{@secret_word}"
     end
+  end
+
+  def play
+    puts "Welcome to Hangman! Try to guess the secret word."
+    puts "The word has #{secret_word.length} letters."
+
+    loop do
+      display_word
+      puts "Remaining guesses: #{@remaining_guesses}"
+      puts "Enter a letter guess:"
+      guess = gets.chomp.downcase
+      handle_guess(guess)
+      break if game_over?
+    end
+
+    display_result
   end
 end
 
-main_game
+hangman_game = Hangman.new('words.txt')
+hangman_game.play
