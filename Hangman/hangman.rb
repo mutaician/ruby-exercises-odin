e01bf191391a996a12a2ae78b53016db540c8abe
mutaician <109066669+mutaician@users.noreply.github.com>
2024-02-12 12:14:33 +0300

    add save and load functionality

--- Hangman/hangman.rb
+++ Hangman/hangman.rb
@@ -1,11 +1,64 @@
+require 'json'
+require 'fileutils'
+
 class Hangman
   attr_reader :secret_word, :guessed_letters, :remaining_guesses
+  GAME_SAVES_DIR = FileUtils.mkdir_p('saved_games')
 
   def initialize(words_file_path)
     @words = read_words_from_file(words_file_path)
     @secret_word = select_random_word()
     @guessed_letters = []
-    @remaining_guesses = 6
+    @remaining_guesses = rand(5..12)
+  end
+
+  def serialize_game_state
+    {
+      secret_word: @secret_word,
+      guessed_letters: @guessed_letters,
+      remaining_guesses: @remaining_guesses
+  }.to_json
+  end
+
+  def load_game(file_path)
+    file_path = File.join(GAME_SAVES_DIR, file_path)
+    begin
+      saved_data = File.read(file_path)
+      game_state = JSON.parse(saved_data)
+      @secret_word = game_state['secret_word']
+      @guessed_letters = game_state['guessed_letters']
+      @remaining_guesses = game_state['remaining_guesses']
+      puts "Game loaded from #{file_path}"
+    rescue Errno::ENOENT
+      puts "No saved game found at #{file_path}"
+      puts "Starting a new game"
+      sleep(2)
+      return
+    rescue => error
+      puts "An error occurred while loading the game: #{error.message}"
+      exit(1)
+    end
+  end
+
+  def save_game(file_path)
+    file_path = File.join(GAME_SAVES_DIR, file_path)
+    File.open(file_path, 'w') { |file| file.puts serialize_game_state}
+    puts "Game saved to #{file_path}"
+
+    loop do
+      puts "Do you want to continue playing (C) or quit (Q)?"
+      choice = gets.chomp.downcase
+      case choice
+      when 'c'
+        puts "Continuing the game"
+        break
+      when 'q'
+        puts "Quitting the game"
+        exit
+      else
+        puts "Please enter a valid choice. (C) to continue or (Q) to quit."
+      end
+    end
   end
 
   def read_words_from_file(file_path)
@@ -57,15 +110,41 @@ class Hangman
     end
   end
 
+  def new_game
+    @secret_word = select_random_word()
+    @guessed_letters = []
+    @remaining_guesses = 7
+    puts "New game started."
+  end
+
   def play
-    puts "Welcome to Hangman! Try to guess the secret word."
+    puts "Welcome to Hangman! Would you like to start a new game (N) or load a saved game (L)?"
+    choice = gets.chomp.downcase
+    case choice
+      when 'n'
+        new_game
+      when 'l'
+        puts "Enter a file path/name to load the game:"
+        file_path = gets.chomp
+        load_game(file_path)
+      else
+        puts "Please enter a valid choice. (N) to start a new game or (L) to load a saved game."
+        return
+    end
     puts "The word has #{secret_word.length} letters."
 
     loop do
       display_word
       puts "Remaining guesses: #{@remaining_guesses}"
-      puts "Enter a letter guess:"
+      puts "Enter a letter guess (or type 'save' to save the game):"
       guess = gets.chomp.downcase
+
+      if guess =='save'
+        puts "Enter a file path to save the game:"
+        file_path = gets.chomp
+        save_game(file_path)
+        next
+      end
       handle_guess(guess)
       break if game_over?
     end
